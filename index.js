4f69abc7245d4c474e57b1b1277d848650016c01
Depaepe Jean-Michel <AJA063@engie.com>
2019-12-02 18:28:17 +0100

    removing unecessary code

--- index.js
+++ index.js
@@ -10,17 +10,8 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-    // change toutes les urls en minuscule
-    .use( (req, res, next) => {
-        req.url = req.url.toLowerCase();
-        next();
-    })
-
     // démarre le framework express.static : sert les fichiers statiques
     .use(express.static(path.join(__dirname, 'public')))
-    
-    // répond à une requête faite sur la racine
-    .get('/', (req, res) => res.send('welcome to epfc-packman'))
     
     // écoute les requêtes HTTP sur le port (5000 ou autre)
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
